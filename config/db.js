38bdb5c3cd08063f0cf66df5b01dbdcb942f2588
kabilan-7-v <kabilanvelmani@gmail.com>
2025-04-25 22:40:31 +0530

    first

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/tasksDB', {
+    await mongoose.connect('mongodb+srv://kabilanvelmani:Bhe2yxSN2M1RrRag@cluster0.qsuv1z6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
